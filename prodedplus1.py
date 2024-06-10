38eecf89ef56bdb96512d8bad3f3a264e4a906e6
rohit1208-github <89534231+rohit1208-github@users.noreply.github.com>
2024-06-10 14:56:29 +0530

    Add files via upload

---
+++ prodedplus1.py
@@ -0,0 +1,180 @@
+import streamlit as st
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import PyPDFLoader
+from langchain_community.vectorstores import Chroma
+from langchain_community.embeddings import GPT4AllEmbeddings
+from langchain.prompts import PromptTemplate
+from langchain_community.chat_models import ChatOllama
+from langchain_core.output_parsers import JsonOutputParser, StrOutputParser
+from typing_extensions import TypedDict
+from typing import List
+from langchain.schema import Document
+from langgraph.graph import END, StateGraph
+
+local_llm = 'llama3'
+pdf_path = r'C:\Users\prohi\Desktop\edplus\Documents\slackfaq.pdf'
+
+loader = PyPDFLoader(pdf_path)
+docs = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+    chunk_size=250,
+    chunk_overlap=0
+)
+
+doc_splits = text_splitter.split_documents(docs)
+
+vectorstore = Chroma.from_documents(
+    documents=doc_splits,
+    collection_name="rag-chroma",
+    embedding=GPT4AllEmbeddings(),
+)
+
+retriever = vectorstore.as_retriever()
+
+llm = ChatOllama(model=local_llm, format="json", temperature=0)
+
+prompt = PromptTemplate(
+    template="""<|begin_of_text|><|start_header_id|>system<|end_header_id|> You are a grader assessing relevance 
+    of a retrieved document to a user question. If the document contains keywords related to the user question, 
+    grade it as relevant. It does not need to be a stringent test. The goal is to filter out erroneous retrievals. \n
+    Give a binary score 'yes' or 'no' score to indicate whether the document is relevant to the question. \n
+    Provide the binary score as a JSON with a single key 'score' and no preamble or explanation.
+     <|eot_id|><|start_header_id|>user<|end_header_id|>
+    Here is the retrieved document: \n\n {document} \n\n
+    Here is the user question: {question} \n <|eot_id|><|start_header_id|>assistant<|end_header_id|>
+    """,
+    input_variables=["question", "document"],
+)
+
+retrieval_grader = prompt | llm | JsonOutputParser()
+
+prompt = PromptTemplate(
+    template="""<|begin_of_text|><|start_header_id|>system<|end_header_id|> You are an assistant for question-answering tasks. 
+    Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. 
+    Use three sentences maximum and keep the answer concise <|eot_id|><|start_header_id|>user<|end_header_id|>
+    Question: {question} 
+    Context: {context} 
+    Answer: <|eot_id|><|start_header_id|>assistant<|end_header_id|>""",
+    input_variables=["question", "document"],
+)
+
+llm = ChatOllama(model=local_llm, temperature=0)
+
+rag_chain = prompt | llm | StrOutputParser()
+
+llm = ChatOllama(model=local_llm, format="json", temperature=0)
+
+prompt = PromptTemplate(
+    template=""" <|begin_of_text|><|start_header_id|>system<|end_header_id|> You are a grader assessing whether 
+    an answer is grounded in / supported by a set of facts. Give a binary 'yes' or 'no' score to indicate 
+    whether the answer is grounded in / supported by a set of facts. Provide the binary score as a JSON with a 
+    single key 'score' and no preamble or explanation. <|eot_id|><|start_header_id|>user<|end_header_id|>
+    Here are the facts:
+    \n ------- \n
+    {documents} 
+    \n ------- \n
+    Here is the answer: {generation}  <|eot_id|><|start_header_id|>assistant<|end_header_id|>""",
+    input_variables=["generation", "documents"],
+)
+
+hallucination_grader = prompt | llm | JsonOutputParser()
+
+prompt = PromptTemplate(
+    template="""<|begin_of_text|><|start_header_id|>system<|end_header_id|> You are a grader assessing whether an 
+    answer is useful to resolve a question. Give a binary score 'yes' or 'no' to indicate whether the answer is 
+    useful to resolve a question. Provide the binary score as a JSON with a single key 'score' and no preamble or explanation.
+     <|eot_id|><|start_header_id|>user<|end_header_id|> Here is the answer:
+    \n ------- \n
+    {generation} 
+    \n ------- \n
+    Here is the question: {question} <|eot_id|><|start_header_id|>assistant<|end_header_id|>""",
+    input_variables=["generation", "question"],
+)
+
+answer_grader = prompt | llm | JsonOutputParser()
+
+class GraphState(TypedDict):
+    question : str
+    generation : str
+    documents : List[str]
+
+def retrieve(state):
+    print("---RETRIEVE---")
+    question = state["question"]
+    documents = retriever.invoke(question)
+    return {"documents": documents, "question": question}
+
+def generate(state):
+    print("---GENERATE---")
+    question = state["question"]
+    documents = state["documents"]
+    generation = rag_chain.invoke({"context": documents, "question": question})
+    return {"documents": documents, "question": question, "generation": generation}
+
+def grade_documents(state):
+    print("---CHECK DOCUMENT RELEVANCE TO QUESTION---")
+    question = state["question"]
+    documents = state["documents"]
+    filtered_docs = []
+    for d in documents:
+        score = retrieval_grader.invoke({"question": question, "document": d.page_content})
+        grade = score['score']
+        if grade.lower() == "yes":
+            print("---GRADE: DOCUMENT RELEVANT---")
+            filtered_docs.append(d)
+        else:
+            print("---GRADE: DOCUMENT NOT RELEVANT---")
+            continue
+    return {"documents": filtered_docs, "question": question}
+
+def grade_generation_v_documents_and_question(state):
+    print("---CHECK HALLUCINATIONS---")
+    question = state["question"]
+    documents = state["documents"]
+    generation = state["generation"]
+    score = hallucination_grader.invoke({"documents": documents, "generation": generation})
+    grade = score['score']
+    if grade == "yes":
+        print("---DECISION: GENERATION IS GROUNDED IN DOCUMENTS---")
+        print("---GRADE GENERATION vs QUESTION---")
+        score = answer_grader.invoke({"question": question,"generation": generation})
+        grade = score['score']
+        if grade == "yes":
+            print("---DECISION: GENERATION ADDRESSES QUESTION---")
+            return "useful"
+        else:
+            print("---DECISION: GENERATION DOES NOT ADDRESS QUESTION---")
+            return "not useful"
+    else:
+        print("---DECISION: GENERATION IS NOT GROUNDED IN DOCUMENTS, RE-TRY---")
+        return "not supported"
+
+workflow = StateGraph(GraphState)
+workflow.add_node("retrieve", retrieve)
+workflow.add_node("grade_documents", grade_documents) 
+workflow.add_node("generate", generate)
+workflow.set_entry_point("retrieve")
+workflow.add_edge("retrieve", "grade_documents")
+workflow.add_edge("grade_documents", "generate")
+workflow.add_conditional_edges(
+    "generate",
+    grade_generation_v_documents_and_question,
+    {
+        "not supported": "generate",
+        "useful": END,
+        "not useful": "retrieve",
+    },
+)
+app = workflow.compile()
+
+st.title("Enrollment Center ASU")
+question = st.text_input("How can we assist you today?")
+
+if st.button("Get Answer"):
+    inputs = {"question": question}
+    for output in app.stream(inputs):
+        for key, value in output.items():
+            print(f"Finished running: {key}:")
+            if key == "generate":
+                st.write(f"Answer: {value['generation']}")
